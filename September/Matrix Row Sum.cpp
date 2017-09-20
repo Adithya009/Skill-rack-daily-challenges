b49a5d29d098ecfd253e0886d305bfd2acc52585
Arieswaran <arieswaran@gmail.com>
2017-09-20 07:47:30 +0530

    20-Sep-2017
    This program runs successfully

---
+++ September/Matrix Row Sum.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+ 
+using namespace std;
+
+int main()
+{
+    int r,c,s=0;
+    cin>>r>>c;
+    int a[r][c];
+    for(int i=0;i<r;i++)
+     for(int j=0;j<c;j++)
+      cin>>a[i][j];
+    for(int i=0;i<r;i++)
+    {
+        s=0;
+        for(int j=0;j<c;j++)
+        s+=a[i][j];
+        cout<<s<<endl;
+    }
+    return 0;
+
+
+}
