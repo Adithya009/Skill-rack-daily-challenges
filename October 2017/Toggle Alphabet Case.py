a4e3686616f88cadd09ee99f45a1aad546d68576
Arieswaran <arieswaran@gmail.com>
2017-10-10 10:57:10 +0530

    10-Oct-2017
    This program runs successfully

---
+++ October 2017/Toggle Alphabet Case.py
@@ -0,0 +1,6 @@
+#Arieswaran
+#easy
+s=raw_input().strip()   #getting the input string 
+t=s.swapcase()          #swaping the case 
+a=s+"-"+t               #concatinating the strings 
+print a                 #printing "string-swaped_Case_String"
